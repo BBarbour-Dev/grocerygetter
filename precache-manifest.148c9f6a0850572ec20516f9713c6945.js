1fb6335add8929c5ee539a909d9712255ddcea7c
steelvoltage <steelvoltage@gmail.com>
2019-03-02 08:29:42 -0500

    Updates

---
+++ precache-manifest.148c9f6a0850572ec20516f9713c6945.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e12388f55c60f4f87a16",
+    "url": "/grocerygetter/static/js/runtime~main.e12388f5.js"
+  },
+  {
+    "revision": "37a932d28e0f6c5d06c8",
+    "url": "/grocerygetter/static/js/main.37a932d2.chunk.js"
+  },
+  {
+    "revision": "0c806c039a87efc49450",
+    "url": "/grocerygetter/static/js/2.0c806c03.chunk.js"
+  },
+  {
+    "revision": "37a932d28e0f6c5d06c8",
+    "url": "/grocerygetter/static/css/main.28854330.chunk.css"
+  },
+  {
+    "revision": "0ca792cf079bcb304724bd2c13228bda",
+    "url": "/grocerygetter/index.html"
+  }
+];
